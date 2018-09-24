a12aa8fbf40394863013de3f81bd42b21769dec9
AnErickson <42577508+AnErickson@users.noreply.github.com>
2018-09-24 14:42:35 -0600

    Add files via upload

---
+++ Chapter4/FourSeventeen.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+class FourSeventeen {
+	public static void main(String[] args) {
+		
+	}
+}
+
+	// Andrew Erickson Fri Sept 21 2018
+
+	Scanner input = new Scanner(System.in);
+	
+	int Jan = 31;
+	int Feb = 28;
+	int Mar = 31;
+	int Apr = 30;
+	int May = 31;
+	int Jun = 31;
+	int Jul = 30;
+	int Aug = 31;
+	int Sep = 30;
+	int Oct = 31;
+	int Nov = 30;
+	int Dec = 31;
+	
+	
+	System.out.println ("Enter a year");
+	
+	double year = input.nextDouble();
+	
+	System.out.println ("Enter a month");
+	
+	double month = input.nextDouble();
+	
+	//not equal to !=
+	//String -> !mionth.equals()
+	
+	}
+	if (year % 4 == 0){
+		
+		if (year % 100 == 0){
+			
+			if (year % 400 == 0)
+			
+			Feb = 29
+
+		}
+		
+	}
+	if (month.equals("Jan") ) || month.equals("March")) {
+			System.out.println(month + " " + year + "has 31 days"
+		}
+		
+		else if (month.equals("Feb")){
+		
+		}
+		
+		else{
+			System.out.println("Invalid month")
+		}
+
+		System.out.println()
+		
+		//assign strings and variable names together
+
